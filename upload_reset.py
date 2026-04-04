25760c2b6d0f277d309d8d278013ed731251d239
Diosives Pedro Nunes Crobute <diolindo819@gmail.com>
2026-04-04 10:26:29 +0000

    atualizando

--- upload_reset.py
+++ upload_reset.py
@@ -6,18 +6,35 @@ FTP_USER = 'if0_41574650'
 FTP_PASS = '0svEjAnMHnX'
 
 php_code = """<?php
+require_once 'core/config.php';
 require_once 'core/Database.php';
-$db = Database::getInstance();
-$hash = '$2y$10$Z2gZWA9tC9q1JEkOGbtz1u3BqOTc1B6X3.JqabWipEBXADqIotyL.';
-$stmt = $db->prepare("UPDATE utilizadores SET email='diretor_ghs@gmail.com', senha=:hash WHERE tipo='admin'");
-$stmt->execute([':hash' => $hash]);
-echo "<h1>CREDENCIAS ATUALIZADAS!</h1><p>Pode fechar esta aba e tentar fazer o login.</p>";
+
+try {
+    $db = Database::getInstance();
+    $hash = '$2y$10$Z2gZWA9tC9q1JEkOGbtz1u3BqOTc1B6X3.JqabWipEBXADqIotyL.';
+    
+    // Tentar atualizar
+    $stmt = $db->prepare("UPDATE utilizadores SET email='diretor_ghs@gmail.com', senha=? WHERE tipo='admin'");
+    $stmt->execute([$hash]);
+
+    echo "<div style='font-family:sans-serif; text-align:center; margin-top:50px;'>";
+    echo "<h1 style='color:green;'>CREDENCIAS ATUALIZADAS!</h1>";
+    echo "<p>Agora use:<br>E-mail: <b>diretor_ghs@gmail.com</b><br>Senha: <b>samba_ghs@2007</b></p>";
+    echo "<a href='/'>Ir para o Login</a>";
+    echo "</div>";
+
+} catch (Exception $e) {
+    echo "<h1>Erro Técnico</h1>: " . $e->getMessage();
+}
 @unlink(__FILE__);
 """
 
-ftp = ftplib.FTP(FTP_HOST)
-ftp.login(FTP_USER, FTP_PASS)
-ftp.cwd('/htdocs')
-ftp.storbinary('STOR reset_admin.php', io.BytesIO(php_code.encode('utf-8')))
-ftp.quit()
-print("RESET FILE UPLOADED")
+try:
+    ftp = ftplib.FTP(FTP_HOST)
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.cwd('/htdocs')
+    ftp.storbinary('STOR reset_admin.php', io.BytesIO(php_code.encode('utf-8')))
+    ftp.quit()
+    print("Sucesso!")
+except Exception as e:
+    print("ERRO:", e)
